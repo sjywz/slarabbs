75158fb909c3050f2a9f12694fe78fff1a44eaf6
vi@lzhy <ysjywz@gmail.com>
2022-01-27 14:16:07 +0800

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
-        'introduction'
+        'introduction',
+        'avatar'
     ];
 
     /**
