316f7a2117c7f1ee59ec360ac7208a6377b6b33f
柳周侯爷 <sjywz@foxmail.com>
2022-04-08 16:26:27 +0800

    添加删除编辑判断

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -45,20 +45,23 @@
 
           <div class="operate">
             <hr>
-            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-edit"></i> 编辑
-            </a>
-            <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
-              style="display: inline-block;"
-              onsubmit="return confirm('您确定要删除吗？');">
-              {{ csrf_field() }}
-              {{ method_field('DELETE') }}
-              <button type="submit" class="btn btn-outline-secondary btn-sm">
-                <i class="far fa-trash-alt"></i> 删除
-              </button>
-            </form>
+            @can('update', $topic)
+              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> 编辑
+              </a>
+            @endcan
+            @can('destroy', $topic)
+              <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                style="display: inline-block;"
+                onsubmit="return confirm('您确定要删除吗？');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+              </form>
+            @endcan
           </div>
-
         </div>
       </div>
     </div>
